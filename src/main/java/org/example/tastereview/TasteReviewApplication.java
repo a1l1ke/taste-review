ce612c7e719484fd00e999195c4466532823d2ed
a1l1ke <onetenthplease@gmail.com>
2026-09-17 14:04:04 +0900

    Initial commit

---
+++ src/main/java/org/example/tastereview/TasteReviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.tastereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tastereview/TasteReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tastereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
